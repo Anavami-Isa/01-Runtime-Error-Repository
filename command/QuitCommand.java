8c9ccd294e9601f64c8b2f3139246a80172e5b8d
Danielfa11 <danielfa@email.sc.edu>
2022-02-27 17:33:22 -0500

    added the quit comand

--- command/QuitCommand.java
+++ command/QuitCommand.java
@@ -1,13 +1,14 @@
 package command;
 
-public class QuitCommand {
+public class QuitCommand implements Command {
     private Player player;
 
     public QuitCommand(Player player) {
-
+        this.player = player;
     }
 
     public void execute() {
-        
+       System.out.println("Gamne Over");
+        System.exit(0);
     }
 }
