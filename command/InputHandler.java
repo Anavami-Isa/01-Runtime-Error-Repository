1b899f498691a3e2803ed8973d8a659c7a3c266e
scottlesley <smlesley@email.sc.edu>
2022-02-27 23:46:29 -0500

    Added work for the InputHandler

--- command/InputHandler.java
+++ command/InputHandler.java
@@ -4,10 +4,18 @@ public class InputHandler {
     private HashMap<String, command> commands;
 
     public inputHandler(Player player) {
-
+        commands = new Hashmap<>();
+        RunCommand run = new RunCommand(player);
+        JumpCommand jump = new JumpCommand(player);
+        FireCommand fire = new FireCommand(player);
+        QuitCommand quit = new QuitCommand(player);
+        this.commands.put("run", run);
+        this.commands.put("jump", run);
+        this.commands.put("fire", run);
+        this.commands.put("quit", run);
     }
 
     public void buttonPressed(String button) {
-
+        commands.get(button).execute();
     }
 }
