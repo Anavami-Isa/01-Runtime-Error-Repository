1f40c0ea03302d4d8033e9377ea40a20c2c2f784
Gazy Rubied <grubied@email.sc.edu>
2022-02-27 23:27:46 -0500

    added class Fire

--- command/FireCommand.java
+++ command/FireCommand.java
@@ -1,55 +1,36 @@
 package command;
-
+import java.util.scanner;
 import java.util.concurrent.TimeUnit;
-import java.util.*;
-
-/**
- * This class executes the fire command
- * @author Anavami Isaa
- */
-public class FireCommand {
+public class JumpCommand {
     private Player player;
 
-    /**
-     * This method instantiates the player
-     * @param player the player that the fire command will be executed on
-     */
-    public FireCommand(Player player) {
-        this.player = player;
+    public JumpCommand(Player player) {
+    this.fire = fire;
     }
+    File Fire = new File("Desktop/Fire.txt");
+    Scanner scan = new scanner(Fire);{
 
-    /**
-     * This method executes the fire command
-     */
-    public void execute() {
-        File file = new File("fire.txt");
-        Scanner read = new Scanner(file);
-        while (read.hasNextLine()) {
-            for (int i = 0; i < 3; i++) {
-                String data = read.nextLine();
-                System.out.println(data);
-            }
-            sleep(1);
-            clear();
-        }
-    }
-
-    /**
-     * This method pauses the actions for a brief peroid of time
-     * @param num the amount of time to pause
-     */
-    private void sleep(int num) {
-        try {
-            TimeUnit.MILLISECONDS.sleep(num);
-        } catch (Exception e) {
-            System.out.println("Timmer error");
-        }
-    }
     
-    /**
-     * This method clears the print line
-     */
-    private void clear() {
-        System.out.print("\033[H\033[2J");
+    while(readFile.hasNextLine()) {
+
+    String toPrint[] = new String [3];
+    for (int i = 0; i< 3; i++){
+        toPrint[i]= readFile.nextLine();
+         }
+                   }
+                Fre.txt.close();{
+                    for (int i = 0; i < 3; i++);
+                    System.out.println(toPrint[i]);
+                    sleep();
+                    clear();
+        }
+            } 
+            public void execute() {
+                Fire();
+        
+    }
+    public String getName(){
+        return "fire";
     }
 }
+
--- command/JumpCommand.java
+++ command/JumpCommand.java
@@ -1,13 +1,39 @@
 package command;
-
-public class JumpCommand implements Command {
+import java.util*;
+import java.util.concurrent.TimeUnit;
+public class JumpCommand {
     private Player player;
 
     public JumpCommand(Player player) {
-    this.player = player;
+    this.jump = jump;
+    }
+    File Jump = new File("Desktop/Jump.txt");
+    Scanner scan = new scanner(Jump);{
+
+    
+    while(readFile.hasNextLine()) {
+
+    String toPrint[] = new String [6];
+    for (int i = 0; i< 6; i++){
+        toPrint[i]= readFile.nextLine();
+           }
+        Jump.txt.close();{
+            for (int i = 0; i < 6; i++);
+            System.out.println(toPrint[i]);
+            sleep();
+            clear();
+
+        }
+        
+        }
+            } 
+    public void execute() {
+        Jump();
     }
 
-    public void execute() {
-        player.jump();
+    public String getName() {
+        return "Jump";
     }
+
 }
+
